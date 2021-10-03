5220a4276f2eb20f1949a67396c3ac6462ad9c2c
노승은 <noseun101@gmail.com>
2021-10-03 17:46:20 +0900

    Initial Commit

---
+++ baisc/variable.py
@@ -0,0 +1,9 @@
+identify = '지구인'
+number_of_legs = 4
+print('안녕!나는', identify, '이야. 나는 다리가', number_of_legs,'개 있어')
+
+identify = '한국인'
+number_of_legs = 2
+print('안녕!나는', identify, '이야. 나는 다리가', number_of_legs,'개 있어')
+
+
