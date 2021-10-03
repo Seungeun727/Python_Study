9802da3ee0c1cea08f3d2b12e57ae052accd47b2
노승은 <noseun101@gmail.com>
2021-10-03 18:26:50 +0900

    Shell 사용법

---
+++ baisc/comment2.py
@@ -0,0 +1,13 @@
+# 주석:코드 해석을 위한 설명
+# 여러 내용을 포함한 주석은 """으로 표시한다.
+identify = '지구인'    # 종
+number_of_legs = 4   # 다리의 개수
+# number_of_legs = 4 
+print('안녕!나는', identify, '이야. 나는 다리가', number_of_legs,'개 있어')
+
+identify = '한국인'
+number_of_legs = 2
+print('안녕!나는', identify, '이야. 나는 다리가', number_of_legs,'개 있어')
+
+
+
